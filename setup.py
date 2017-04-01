4b589e68fcb9f1c5c87293d6d6a8de6c6bd326c9
Lawrence Thatcher <lwthatcher@msn.com>
2017-04-01 14:40:21 -0600

    adding setup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,13 @@
+"""Setup for GFMM"""
+
+from distutils.core import setup
+
+setup(
+    name='vat-formatter',
+    version='0.0.1',
+    description='Takes data and labels from VAT and transforms them into a different format.',
+    author='Lawrence Thatcher',
+    author_email='lwthatcher@msn.com',
+    url='https://github.com/lwthatcher/mlp',
+    packages=['formatter']
+)
