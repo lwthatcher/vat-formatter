4b4eb46e590648c38b9cdd1c6ea2342dc0688ced
Lawrence Thatcher <lwthatcher@msn.com>
2017-04-01 19:54:43 -0600

    added num_features and num_examples properties

--- formatter/formatter.py
+++ formatter/formatter.py
@@ -1,4 +1,4 @@
-import numpy
+import numpy as np
 import argparse
 import json
 import csv
@@ -8,7 +8,8 @@ class SyncFormatter:
 
     MILLISECONDS = 1000.
 
-    def __init__(self, data_file, labels_file, event_types_file=None, tps=512.):
+    def __init__(self, data_file, labels_file, event_types_file=None, tps=512., sensors=('A', 'G'),
+                 format_type="simple"):
         # ticks per second and conversion
         self.ticks_per_second = tps
         self.conversion = self.MILLISECONDS / tps
@@ -17,6 +18,7 @@ class SyncFormatter:
         # save data/labels ...?
         self._data_file = data_file
         self._parsed_labels = parsed_labels
+        self._sensors = sensors
         # get event-name-map
         if event_types_file:
             parsed_event_types = self.parse_json(event_types_file)
@@ -27,21 +29,25 @@ class SyncFormatter:
         data_flash = int(self.red_flash__data(data_file) * self.conversion)
         label_flash = int(self.red_flash__labels(parsed_labels) * self.MILLISECONDS)
         self.offset = label_flash - data_flash
-        # get label times
-        self.label_times = self.parse_labels(parsed_labels)
+        # parse labels
+        self.label_times = np.array(self.parse_labels(parsed_labels))
+        # parse data
+        self.data = self.parse_csv(data_file, sensors)
 
-    def parse_csv(self, data_file):
+    def parse_csv(self, data_file, sensors):
         signals = {}
+        for sensor in sensors:
+            signals[sensor] = []
         with open(data_file, newline='') as csv_file:
             data_reader = csv.reader(csv_file)
             for line in data_reader:
                 if len(line) > 0:
-                    token, dimensions = self.parse_line(line, self.conversion)
-                    if token is not None and dimensions is not None:
-                        if token not in signals:
-                            signals[token] = []
+                    if line[0] in signals and len(line) == 5:
+                        token = line[0]
+                        dimensions = [int(i) for i in line[1:]]
+                        dimensions[0] = int(dimensions[0] * self.conversion)
                         signals[token].append(dimensions)
-        return signals
+        return [np.array(signals[dim]) for dim in sensors]
 
     def parse_labels(self, parsed_labels):
         lbl_times = []
@@ -61,14 +67,21 @@ class SyncFormatter:
                 lbl_times.append([start, end, self.event_names[name]])
         return lbl_times
 
+    # def format_simple(self):
+    #     # can only use this format type if all signals are of equal length
+    #     if all([len(x) == len(y) for x in self.data for y in self.data]):
+    #         print("cannot use this format if signals are not of the same length")
+    #         return None
+    #     feature_names = self.get_feature_names(self._sensors)
 
-    @staticmethod
-    def parse_line(line, conversion):
-        # TODO: add further checks, like in the original VAT
-        token = line[0]
-        line[1] = str(conversion * int(line[1]))  # do conversion, if needed
-        dimensions = line[1:]
-        return token, dimensions
+
+    @property
+    def num_features(self):
+        return len(self._sensors) * 3
+
+    @property
+    def num_examples(self):
+        return len(self.data[0])
 
     @staticmethod
     def red_flash__data(data_file):
@@ -112,20 +125,31 @@ class SyncFormatter:
                             n += 1
         return result
 
+    @staticmethod
+    def get_feature_names(sensors):
+        feature_names = []
+        for dim in sensors:
+            feature_names.append(dim + ".x")
+            feature_names.append(dim + ".y")
+            feature_names.append(dim + ".z")
+        return feature_names
 
 def parse_args(_args=None):
     parser = argparse.ArgumentParser(description='Format that data!')
     parser.add_argument('data', help='file containing data')
     parser.add_argument('labels', help='file containing labels')
     parser.add_argument('--event_types', '-e', help='the event_types.json file')
+    parser.add_argument('--ticks_per_second', '-tps', type=int, default=512,
+                        help='the number of ticks per second the data file uses')
     if _args is None:
-        return parser.parse_args()
-    return parser.parse_args(_args)
+        parsed_args = parser.parse_args()
+    parsed_args = parser.parse_args(_args)
+    return parsed_args
 
 
 if __name__ == '__main__':
     args = parse_args()
-    formatter = SyncFormatter(args.data, args.labels, args.event_types)
+    formatter = SyncFormatter(args.data, args.labels, args.event_types, tps=args.ticks_per_second)
     print("offset:", formatter.offset)
     print("event-name-map", formatter.event_names)
     print("label times", formatter.label_times)
