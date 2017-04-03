2ddee2bfb32121273786e105c524d28dc065a682
Lawrence Thatcher <lwthatcher@msn.com>
2017-04-03 14:06:41 -0600

    stubbed out normalizer class

---
+++ formatter/normalizer.py
@@ -0,0 +1,12 @@
+"""Module for normalizing sensor data, to make it easier to learn"""
+import numpy as np
+
+
+class Normalizer:
+
+    def max_per_sensor(self, sensors=('A', 'G')):
+        pass
+
+
+if __name__ == '__main__':
+    pass
