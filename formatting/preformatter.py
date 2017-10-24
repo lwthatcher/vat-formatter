82f53695cc8e540354b5f96df0c79de9fa8fc0e1
Lawrence Thatcher <lwthatcher@msn.com>
2017-10-23 18:22:11 -0600

    can convert data file to numpy array

--- formatting/preformatter.py
+++ formatting/preformatter.py
@@ -30,13 +30,37 @@ class DataPreFormatter:
 
     def __init__(self, data_file, sensors=('A', 'G'), tps=512.):
         self.data = parse_csv(data_file, sensors, tps)
+        print('loaded data')
+
+    @property
+    def examples(self):
+        return [len(x) for x in self.data]
+
+    @property
+    def features(self):
+        return [x.shape[1] - 1 for x in self.data]
     
-    def convert():
-        pass
+    def convert(self):
+        # can only use this format type if all signals are of equal length
+        if not all([len(x) == len(y) for x in self.data for y in self.data]):
+            print("cannot use this format if signals are not of the same length")
+            return None
+        # create matrix
+        rows = np.max(self.examples)
+        cols = np.sum(self.features)
+        result = np.zeros((rows, cols))
+        # add x,y,z for each signal
+        j = 0
+        for i, f in enumerate(self.features):
+            result[:, j:j+f] = self.data[i][:, 1:]
+            j = (i+1) * f
+        return result
+
 
 def parse_args(_args=None):
     parser = argparse.ArgumentParser(description='Formatting for data without labels')
     parser.add_argument('data', help='data file to convert')
+    parser.add_argument('--output', '-O', help='the output file to save to')
     # parse arguments
     if _args is None:
         __args = parser.parse_args()
@@ -45,6 +69,10 @@ def parse_args(_args=None):
     # return results
     return __args
 
+
 if __name__ == '__main__':
     args = parse_args()
     formatter = DataPreFormatter(args.data)
+    result = formatter.convert()
+    out = args.output or 'output.npy'
+    np.save(out, result)
